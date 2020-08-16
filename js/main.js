4865cd9d75bb1caa3f20101bcb2beb5973a0aecd
kylelee0617 <kyle.lee06172@gmail.com>
2020-08-17 05:54:59 +0800

    小修改

--- js/main.js
+++ js/main.js
@@ -1,67 +1,77 @@
-$(function(){
-    let tmpOtherSub = "<div class=\"swiper-slide swiper-slide-active\" style=\"width: 235.167px; margin-right: 20px;\">"
-                    +"<div class=\"movie-item\" cid=\"1471\" ctype=\"2\">"
-                    +"<h6 class=\"tag-top\"><span>{syncDesc}</span></h6>"
-                    +"<a href=\"#\" title=\"{name}\">"
-                    +"<div class=\"img-block\">"
-                    +"<img width=\"100%\" class=\"swiper-lazy img-responsive swiper-lazy-loaded\" alt=\"{name}\" src=\"{img}\">"
-                    +"<h6 class=\"tag-btm\"><span class=\"tag-free\">免費</span></h6>"
-                    +"</div>"
-                    +"<div class=\"info\">"
-                    +"<h5>{name}</h5>"
-                    +"<p class=\"note\">2020 / 台灣</p>"
-                    +"</div>"
-                    +"</a>"
-                    +"<div class=\"btn-box\"><a class=\"btn btn-default autoplay\" href=\"#\" ctype=\"2\" cid=\"1471\">立即觀看</a></div>"
-                    +"</div></div>";
-    
-    let tmpOtherSection = "<div class=\"container-fluid\">"
-                    +"<div class=\"row movie-block\">"
-                    +"<div class=\"col-xs-12\">"
-                    +"<h2 class=\"section-title\">{title}</h2><a href=\"https://video.friday.tw/latest\" class=\"more pull-right\">更多</a>"
-                    +"</div>"
-                    +"<div class=\"swiper-container gallery-movie instance-0 swiper-container-horizontal\">"
-                    +"<div class=\"swiper-wrapper\">"
-                    +"</div>"
-                    +"</div>"
-                    +"<div class=\"swiper-button-prev swiper-button-white btn-prev-0 swiper-button-disabled\"></div>"
-                    +"<div class=\"swiper-button-next swiper-button-white btn-next-0\"></div>"
-                    +"</div>"
-                    +"</div>"
+$(function () {
+  let tmpOtherSub =
+    '<div class="swiper-slide swiper-slide-active" style="width: 235.167px; margin-right: 20px;">' +
+    '<div class="movie-item" cid="1471" ctype="2">' +
+    '<h6 class="tag-top"><span>{syncDesc}</span></h6>' +
+    '<a href="#" title="{name}">' +
+    '<div class="img-block">' +
+    '<img width="100%" class="swiper-lazy img-responsive swiper-lazy-loaded" alt="{name}" src="{img}">' +
+    '<h6 class="tag-btm"><span class="tag-free">免費</span></h6>' +
+    "</div>" +
+    '<div class="info">' +
+    "<h5>{name}</h5>" +
+    '<p class="note">2020 / 台灣</p>' +
+    "</div>" +
+    "</a>" +
+    '<div class="btn-box"><a class="btn btn-default autoplay" href="#" ctype="2" cid="1471">立即觀看</a></div>' +
+    "</div></div>";
 
-    $.each(sectionList , (index , data) => {
-        if(data != null){
-            
-            if(data.code == "class") {
+  let tmpOtherSection =
+    '<div class="container-fluid">' +
+    '<div class="row movie-block">' +
+    '<div class="col-xs-12">' +
+    '<h2 class="section-title">{title}</h2><a href="https://video.friday.tw/latest" class="more pull-right">更多</a>' +
+    "</div>" +
+    '<div class="swiper-container gallery-movie instance-0 swiper-container-horizontal">' +
+    '<div class="swiper-wrapper">' +
+    "</div>" +
+    "</div>" +
+    '<div class="swiper-button-prev swiper-button-white btn-prev-0 swiper-button-disabled"></div>' +
+    '<div class="swiper-button-next swiper-button-white btn-next-0"></div>' +
+    "</div>" +
+    "</div>";
 
-            }else if(data.code == "5G"){
+  $.each(sectionList, (index, data) => {
+    // if(data != null){
+    //     if(data.code == "class") {
 
-            }else {
-                doOther(data);
-            }
-            
-        }
-    })
+    //     }else if(data.code == "5G"){
 
-    function doOther(data) {
-        const code = data.code;
-        
-        //載入大框
-        let section = $("#" + code);
-        section.html(tmpOtherSection.replace(/{title}/g , data.name))
+    //     }else {
+    //         doOther(data);
+    //     }
+    // }
 
-        //載入子元素
-        let lastHtml = "";
-        $.each(data.sub , (index, subData) => {
-            lastHtml += tmpOtherSub.replace(/{name}/g , subData.name)
-                                .replace(/{img}/g , "images/" + code + "/" + subData.img)
-                                .replace(/{syncDesc}/g , "");
-        })
-
-        let sectionSub = $("#" + code + " .swiper-wrapper");
-        sectionSub.html(lastHtml)
+    //switch 或 if 擇一
+    switch (data.code) {
+      case "class":
+        console.log("class");
+        break;
+      case "5G":
+        console.log("5G");
+        break;
+      default:
+        doOther(data);
     }
-    
+  });
 
+  function doOther(data) {
+    const code = data.code;
+
+    //載入大框
+    let section = $("#" + code);
+    section.html(tmpOtherSection.replace(/{title}/g, data.name));
+
+    //載入子元素
+    let lastHtml = "";
+    $.each(data.sub, (index, subData) => {
+      lastHtml += tmpOtherSub
+        .replace(/{name}/g, subData.name)
+        .replace(/{img}/g, "images/" + code + "/" + subData.img)
+        .replace(/{syncDesc}/g, "");
+    });
+
+    let sectionSub = $("#" + code + " .swiper-wrapper");
+    sectionSub.html(lastHtml);
+  }
 });
-
--- js/sectionList.js
+++ js/sectionList.js
@@ -1,329 +1,382 @@
 const sectionList = [
-    {
-        id: 1,
-        code: "new",
-        name: "最新上線",
-        sub: [
-            {
-                subid:1,
-                code: 11,
-                name: "十匙一犯",
-                img: "1391_3815290_S.jpg",
-            },{
-                subid:2,
-                code: 12,
-                name: "他就是那傢伙",
-                img: "1392_15695467_S.jpg",
-            },{
-                subid:3,
-                code: 13,
-                name: "DD",
-                img: "1400_3553516_S.jpg",
-            },{
-                subid:4,
-                code: 14,
-                name: "name14",
-                img: "1422_324242_S.jpg",
-            },{
-                subid:5,
-                code: 15,
-                name: "我們有點不對勁",
-                img: "1472_2484952_S.jpg",
-            },{
-                subid:6,
-                code: 16,
-                name: "放飛大丈夫",
-                img: "58294_8161183_S.jpg",
-            },{
-                subid:7,
-                code: 16,
-                name: "好片",
-                img: "58425_2064638_S.jpg",
-            },{
-                subid:8,
-                code: 16,
-                name: "爛片",
-                img: "58503_4626181_S.jpg",
-            }
-        ]
-    }, {
-        id: 2,
-        code: "Korea",
-        name: "韓系物語",
-        sub: [
-            {
-                subid:1,
-                code: 21,
-                name: "韓系1",
-                img: "1371_8377532_S.jpg",
-            },{
-                subid:2,
-                code: 22,
-                name: "韓系2",
-                img: "1393_16610305_S.jpg",
-            },{
-                subid:3,
-                code: 23,
-                name: "韓系3",
-                img: "1427_2057697_S.jpg",
-            },{
-                subid:4,
-                code: 24,
-                name: "韓系4",
-                img: "1428_2265679_S.jpg",
-            },{
-                subid:5,
-                code: 25,
-                name: "韓系5",
-                img: "1441_4266091_S.jpg",
-            },{
-                subid:6,
-                code: 26,
-                name: "韓系6",
-                img: "1456_973947_S.jpg",
-            },{
-                subid:6,
-                code: 26,
-                name: "韓系7",
-                img: "1474_2897109_S.jpg",
-            },{
-                subid:6,
-                code: 26,
-                name: "韓系8",
-                img: "54671_459119_S.jpg",
-            }
-        ]
-    }, {
-        id: 3,
-        code: "class",
-        name: "經典情懷",
-        sub: [
-            {
-                subid:1,
-                code: 21,
-                name: "name11",
-                img: "1391_3815290_S.jpg",
-            },{
-                subid:2,
-                code: 22,
-                name: "name12",
-                img: "1392_15695467_S.jpg",
-            },{
-                subid:3,
-                code: 23,
-                name: "name13",
-                img: "1400_3553516_S.jpg",
-            },{
-                subid:4,
-                code: 24,
-                name: "name14",
-                img: "1422_324242_S.jpg",
-            },{
-                subid:5,
-                code: 25,
-                name: "name15",
-                img: "1472_2484952_S.jpg",
-            },{
-                subid:6,
-                code: 26,
-                name: "name16",
-                img: "58294_8161183_S.jpg",
-            }
-        ]
-    }, {
-        id: 4,
-        code: "5G",
-        name: "4K IN 5G",
-        sub: [
-            {
-                subid:1,
-                code: 21,
-                name: "name11",
-                img: "56818_19385824_S.jpg",
-            },{
-                subid:2,
-                code: 22,
-                name: "name12",
-                img: "56020_2769105_S.jpg",
-            },{
-                subid:3,
-                code: 23,
-                name: "name13",
-                img: "54482_2980756_S.jpg",
-            },{
-                subid:4,
-                code: 24,
-                name: "name14",
-                img: "56024_2792052_S.jpg",
-            },
-        ]
-    }, {
-        id: 5,
-        code: "newmovies",
-        name: "強檔院線",
-        sub: [
-            {
-                subid:1,
-                code: 21,
-                name: "強檔院線1",
-                img: "56812_17972121_S.jpg",
-            },{
-                subid:2,
-                code: 22,
-                name: "強檔院線2",
-                img: "55874_12527572_S.jpg",
-            },{
-                subid:3,
-                code: 23,
-                name: "強檔院線3",
-                img: "55878_12600270_S.jpg",
-            },{
-                subid:4,
-                code: 24,
-                name: "強檔院線4",
-                img: "56221_75981_S.jpg",
-            },{
-                subid:5,
-                code: 25,
-                name: "強檔院線5",
-                img: "54529_4148299_S.png",
-            },{
-                subid:6,
-                code: 26,
-                name: "強檔院線6",
-                img: "57834_1650363_S.jpg",
-            },{
-                subid:5,
-                code: 25,
-                name: "強檔院線7",
-                img: "56239_17357319_S.jpg",
-            },{
-                subid:6,
-                code: 26,
-                name: "強檔院線8",
-                img: "56814_19526987_S.jpg",
-            }
-        ]
-    }, {
-        id: 6,
-        code: "discount0",
-        name: "0元專區",
-        sub: [
-            {
-                subid:1,
-                code: 21,
-                name: "name11",
-                img: "1392_15695467_S.jpg",
-            },{
-                subid:2,
-                code: 22,
-                name: "name12",
-                img: "1397_16875272_S.jpg",
-            },{
-                subid:3,
-                code: 23,
-                name: "name13",
-                img: "1471_2162890_S.jpg",
-            },{
-                subid:4,
-                code: 24,
-                name: "name14",
-                img: "54760_4038806_S.jpg",
-            },{
-                subid:5,
-                code: 25,
-                name: "name15",
-                img: "1489_378126_S.jpg",
-            },{
-                subid:6,
-                code: 26,
-                name: "name16",
-                img: "54713_2992099_S.jpg",
-            }
-        ]
-    }, {
-        id: 7,
-        code: "driver",
-        name: "無視紅燈!!",
-        sub: [
-            {
-                subid:1,
-                code: 21,
-                name: "name11",
-                img: "4074_654296_S.jpg",
-            },{
-                subid:2,
-                code: 22,
-                name: "name12",
-                img: "13238_8832235_S.jpg",
-            },{
-                subid:3,
-                code: 23,
-                name: "name13",
-                img: "19251_1192286_S.jpg",
-            },{
-                subid:4,
-                code: 24,
-                name: "name14",
-                img: "23889_17830858_S.jpg",
-            },{
-                subid:5,
-                code: 25,
-                name: "name15",
-                img: "27278_106128_S.jpg",
-            },{
-                subid:6,
-                code: 26,
-                name: "name16",
-                img: "54592_5183107_S.jpg",
-            },{
-                subid:5,
-                code: 25,
-                name: "name15",
-                img: "36573_7773977_S.jpg",
-            },{
-                subid:6,
-                code: 26,
-                name: "name16",
-                img: "47148_1334159_S.jpg",
-            }
-        ]
-    }, {
-        id: 8,
-        code: "ghost",
-        name: "鬼怪",
-        sub: [
-            {
-                subid:1,
-                code: 21,
-                name: "name11",
-                img: "1391_3815290_S.jpg",
-            },{
-                subid:2,
-                code: 22,
-                name: "name12",
-                img: "1392_15695467_S.jpg",
-            },{
-                subid:3,
-                code: 23,
-                name: "name13",
-                img: "1400_3553516_S.jpg",
-            },{
-                subid:4,
-                code: 24,
-                name: "name14",
-                img: "1422_324242_S.jpg",
-            },{
-                subid:5,
-                code: 25,
-                name: "name15",
-                img: "1472_2484952_S.jpg",
-            },{
-                subid:6,
-                code: 26,
-                name: "name16",
-                img: "58294_8161183_S.jpg",
-            }
-        ]
-    },
+  {
+    id: 1,
+    code: "new",
+    name: "最新上線",
+    sub: [
+      {
+        subid: 1,
+        code: 11,
+        name: "十匙一犯",
+        img: "1391_3815290_S.jpg",
+      },
+      {
+        subid: 2,
+        code: 12,
+        name: "他就是那傢伙",
+        img: "1392_15695467_S.jpg",
+      },
+      {
+        subid: 3,
+        code: 13,
+        name: "DD",
+        img: "1400_3553516_S.jpg",
+      },
+      {
+        subid: 4,
+        code: 14,
+        name: "name14",
+        img: "1422_324242_S.jpg",
+      },
+      {
+        subid: 5,
+        code: 15,
+        name: "我們有點不對勁",
+        img: "1472_2484952_S.jpg",
+      },
+      {
+        subid: 6,
+        code: 16,
+        name: "放飛大丈夫",
+        img: "58294_8161183_S.jpg",
+      },
+      {
+        subid: 7,
+        code: 16,
+        name: "好片",
+        img: "58425_2064638_S.jpg",
+      },
+      {
+        subid: 8,
+        code: 16,
+        name: "爛片",
+        img: "58503_4626181_S.jpg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    code: "Korea",
+    name: "韓系物語",
+    sub: [
+      {
+        subid: 1,
+        code: 21,
+        name: "韓系1",
+        img: "1371_8377532_S.jpg",
+      },
+      {
+        subid: 2,
+        code: 22,
+        name: "韓系2",
+        img: "1393_16610305_S.jpg",
+      },
+      {
+        subid: 3,
+        code: 23,
+        name: "韓系3",
+        img: "1427_2057697_S.jpg",
+      },
+      {
+        subid: 4,
+        code: 24,
+        name: "韓系4",
+        img: "1428_2265679_S.jpg",
+      },
+      {
+        subid: 5,
+        code: 25,
+        name: "韓系5",
+        img: "1441_4266091_S.jpg",
+      },
+      {
+        subid: 6,
+        code: 26,
+        name: "韓系6",
+        img: "1456_973947_S.jpg",
+      },
+      {
+        subid: 6,
+        code: 26,
+        name: "韓系7",
+        img: "1474_2897109_S.jpg",
+      },
+      {
+        subid: 6,
+        code: 26,
+        name: "韓系8",
+        img: "54671_459119_S.jpg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    code: "class",
+    name: "經典情懷",
+    sub: [
+      {
+        subid: 1,
+        code: 21,
+        name: "name11",
+        img: "1391_3815290_S.jpg",
+      },
+      {
+        subid: 2,
+        code: 22,
+        name: "name12",
+        img: "1392_15695467_S.jpg",
+      },
+      {
+        subid: 3,
+        code: 23,
+        name: "name13",
+        img: "1400_3553516_S.jpg",
+      },
+      {
+        subid: 4,
+        code: 24,
+        name: "name14",
+        img: "1422_324242_S.jpg",
+      },
+      {
+        subid: 5,
+        code: 25,
+        name: "name15",
+        img: "1472_2484952_S.jpg",
+      },
+      {
+        subid: 6,
+        code: 26,
+        name: "name16",
+        img: "58294_8161183_S.jpg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    code: "5G",
+    name: "4K IN 5G",
+    sub: [
+      {
+        subid: 1,
+        code: 21,
+        name: "name11",
+        img: "56818_19385824_S.jpg",
+      },
+      {
+        subid: 2,
+        code: 22,
+        name: "name12",
+        img: "56020_2769105_S.jpg",
+      },
+      {
+        subid: 3,
+        code: 23,
+        name: "name13",
+        img: "54482_2980756_S.jpg",
+      },
+      {
+        subid: 4,
+        code: 24,
+        name: "name14",
+        img: "56024_2792052_S.jpg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    code: "newmovies",
+    name: "強檔院線",
+    sub: [
+      {
+        subid: 1,
+        code: 21,
+        name: "強檔院線1",
+        img: "56812_17972121_S.jpg",
+      },
+      {
+        subid: 2,
+        code: 22,
+        name: "強檔院線2",
+        img: "55874_12527572_S.jpg",
+      },
+      {
+        subid: 3,
+        code: 23,
+        name: "強檔院線3",
+        img: "55878_12600270_S.jpg",
+      },
+      {
+        subid: 4,
+        code: 24,
+        name: "強檔院線4",
+        img: "56221_75981_S.jpg",
+      },
+      {
+        subid: 5,
+        code: 25,
+        name: "強檔院線5",
+        img: "54529_4148299_S.png",
+      },
+      {
+        subid: 6,
+        code: 26,
+        name: "強檔院線6",
+        img: "57834_1650363_S.jpg",
+      },
+      {
+        subid: 5,
+        code: 25,
+        name: "強檔院線7",
+        img: "56239_17357319_S.jpg",
+      },
+      {
+        subid: 6,
+        code: 26,
+        name: "強檔院線8",
+        img: "56814_19526987_S.jpg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    code: "discount0",
+    name: "0元專區",
+    sub: [
+      {
+        subid: 1,
+        code: 21,
+        name: "name11",
+        img: "1392_15695467_S.jpg",
+      },
+      {
+        subid: 2,
+        code: 22,
+        name: "name12",
+        img: "1397_16875272_S.jpg",
+      },
+      {
+        subid: 3,
+        code: 23,
+        name: "name13",
+        img: "1471_2162890_S.jpg",
+      },
+      {
+        subid: 4,
+        code: 24,
+        name: "name14",
+        img: "54760_4038806_S.jpg",
+      },
+      {
+        subid: 5,
+        code: 25,
+        name: "name15",
+        img: "1489_378126_S.jpg",
+      },
+      {
+        subid: 6,
+        code: 26,
+        name: "name16",
+        img: "54713_2992099_S.jpg",
+      },
+    ],
+  },
+  {
+    id: 7,
+    code: "driver",
+    name: "無視紅燈!!",
+    sub: [
+      {
+        subid: 1,
+        code: 21,
+        name: "name11",
+        img: "4074_654296_S.jpg",
+      },
+      {
+        subid: 2,
+        code: 22,
+        name: "name12",
+        img: "13238_8832235_S.jpg",
+      },
+      {
+        subid: 3,
+        code: 23,
+        name: "name13",
+        img: "19251_1192286_S.jpg",
+      },
+      {
+        subid: 4,
+        code: 24,
+        name: "name14",
+        img: "23889_17830858_S.jpg",
+      },
+      {
+        subid: 5,
+        code: 25,
+        name: "name15",
+        img: "27278_106128_S.jpg",
+      },
+      {
+        subid: 6,
+        code: 26,
+        name: "name16",
+        img: "54592_5183107_S.jpg",
+      },
+      {
+        subid: 5,
+        code: 25,
+        name: "name15",
+        img: "36573_7773977_S.jpg",
+      },
+      {
+        subid: 6,
+        code: 26,
+        name: "name16",
+        img: "47148_1334159_S.jpg",
+      },
+    ],
+  },
+  {
+    id: 8,
+    code: "ghost",
+    name: "鬼怪",
+    sub: [
+      {
+        subid: 1,
+        code: 21,
+        name: "name11",
+        img: "1391_3815290_S.jpg",
+      },
+      {
+        subid: 2,
+        code: 22,
+        name: "name12",
+        img: "1392_15695467_S.jpg",
+      },
+      {
+        subid: 3,
+        code: 23,
+        name: "name13",
+        img: "1400_3553516_S.jpg",
+      },
+      {
+        subid: 4,
+        code: 24,
+        name: "name14",
+        img: "1422_324242_S.jpg",
+      },
+      {
+        subid: 5,
+        code: 25,
+        name: "name15",
+        img: "1472_2484952_S.jpg",
+      },
+      {
+        subid: 6,
+        code: 26,
+        name: "name16",
+        img: "58294_8161183_S.jpg",
+      },
+    ],
+  },
 ];
