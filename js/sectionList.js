f66870a4261d858493e725dfd87cf6b9f2464cf1
kylelee0617 <kyle.lee06172@gmail.com>
2020-08-17 05:07:39 +0800

    大綱

---
+++ js/sectionList.js
@@ -0,0 +1,281 @@
+const sectionList = [
+    {
+        id: 1,
+        code: "new",
+        sub: [
+            {
+                subid:1,
+                code: 11,
+                name: "name11",
+                img: "1391_3815290_S.jpg",
+            },{
+                subid:2,
+                code: 12,
+                name: "name12",
+                img: "1392_15695467_S.jpg",
+            },{
+                subid:3,
+                code: 13,
+                name: "name13",
+                img: "1400_3553516_S.jpg",
+            },{
+                subid:4,
+                code: 14,
+                name: "name14",
+                img: "1422_324242_S.jpg",
+            },{
+                subid:5,
+                code: 15,
+                name: "name15",
+                img: "1472_2484952_S.jpg",
+            },{
+                subid:6,
+                code: 16,
+                name: "name16",
+                img: "58294_8161183_S.jpg",
+            }
+        ]
+    }, {
+        id: 2,
+        code: "Korea",
+        sub: [
+            {
+                subid:1,
+                code: 21,
+                name: "name11",
+                img: "1391_3815290_S.jpg",
+            },{
+                subid:2,
+                code: 22,
+                name: "name12",
+                img: "1392_15695467_S.jpg",
+            },{
+                subid:3,
+                code: 23,
+                name: "name13",
+                img: "1400_3553516_S.jpg",
+            },{
+                subid:4,
+                code: 24,
+                name: "name14",
+                img: "1422_324242_S.jpg",
+            },{
+                subid:5,
+                code: 25,
+                name: "name15",
+                img: "1472_2484952_S.jpg",
+            },{
+                subid:6,
+                code: 26,
+                name: "name16",
+                img: "58294_8161183_S.jpg",
+            }
+        ]
+    }, {
+        id: 3,
+        code: "class",
+        sub: [
+            {
+                subid:1,
+                code: 21,
+                name: "name11",
+                img: "1391_3815290_S.jpg",
+            },{
+                subid:2,
+                code: 22,
+                name: "name12",
+                img: "1392_15695467_S.jpg",
+            },{
+                subid:3,
+                code: 23,
+                name: "name13",
+                img: "1400_3553516_S.jpg",
+            },{
+                subid:4,
+                code: 24,
+                name: "name14",
+                img: "1422_324242_S.jpg",
+            },{
+                subid:5,
+                code: 25,
+                name: "name15",
+                img: "1472_2484952_S.jpg",
+            },{
+                subid:6,
+                code: 26,
+                name: "name16",
+                img: "58294_8161183_S.jpg",
+            }
+        ]
+    }, {
+        id: 4,
+        code: "5G",
+        sub: [
+            {
+                subid:1,
+                code: 21,
+                name: "name11",
+                img: "56818_19385824_S.jpg",
+            },{
+                subid:2,
+                code: 22,
+                name: "name12",
+                img: "56020_2769105_S.jpg",
+            },{
+                subid:3,
+                code: 23,
+                name: "name13",
+                img: "54482_2980756_S.jpg",
+            },{
+                subid:4,
+                code: 24,
+                name: "name14",
+                img: "56024_2792052_S.jpg",
+            },
+        ]
+    }, {
+        id: 5,
+        code: "newmovies",
+        sub: [
+            {
+                subid:1,
+                code: 21,
+                name: "name11",
+                img: "56812_17972121_S.jpg",
+            },{
+                subid:2,
+                code: 22,
+                name: "name12",
+                img: "55874_12527572_S.jpg",
+            },{
+                subid:3,
+                code: 23,
+                name: "name13",
+                img: "55878_12600270_S.jpg",
+            },{
+                subid:4,
+                code: 24,
+                name: "name14",
+                img: "56221_75981_S.jpg",
+            },{
+                subid:5,
+                code: 25,
+                name: "name15",
+                img: "54529_4148299_S.jpg",
+            },{
+                subid:6,
+                code: 26,
+                name: "name16",
+                img: "57834_1650363_S.jpg",
+            }
+        ]
+    }, {
+        id: 6,
+        code: "discount0",
+        sub: [
+            {
+                subid:1,
+                code: 21,
+                name: "name11",
+                img: "1392_15695467_S.jpg",
+            },{
+                subid:2,
+                code: 22,
+                name: "name12",
+                img: "1397_16875272_S.jpg",
+            },{
+                subid:3,
+                code: 23,
+                name: "name13",
+                img: "1471_2162890_S.jpg",
+            },{
+                subid:4,
+                code: 24,
+                name: "name14",
+                img: "54760_4038806_S.jpg",
+            },{
+                subid:5,
+                code: 25,
+                name: "name15",
+                img: "1489_378126_S.jpg",
+            },{
+                subid:6,
+                code: 26,
+                name: "name16",
+                img: "54713_2992099_S.jpg",
+            }
+        ]
+    }, {
+        id: 7,
+        code: "dirver",
+        sub: [
+            {
+                subid:1,
+                code: 21,
+                name: "name11",
+                img: "4074_654296_S.jpg",
+            },{
+                subid:2,
+                code: 22,
+                name: "name12",
+                img: "13238_8832235_S.jpg",
+            },{
+                subid:3,
+                code: 23,
+                name: "name13",
+                img: "19251_1192286_S.jpg",
+            },{
+                subid:4,
+                code: 24,
+                name: "name14",
+                img: "23889_17830858_S.jpg",
+            },{
+                subid:5,
+                code: 25,
+                name: "name15",
+                img: "27278_106128_S.jpg",
+            },{
+                subid:6,
+                code: 26,
+                name: "name16",
+                img: "54592_5183107_S.jpg",
+            }
+        ]
+    }, {
+        id: 8,
+        code: "ghost",
+        sub: [
+            {
+                subid:1,
+                code: 21,
+                name: "name11",
+                img: "1391_3815290_S.jpg",
+            },{
+                subid:2,
+                code: 22,
+                name: "name12",
+                img: "1392_15695467_S.jpg",
+            },{
+                subid:3,
+                code: 23,
+                name: "name13",
+                img: "1400_3553516_S.jpg",
+            },{
+                subid:4,
+                code: 24,
+                name: "name14",
+                img: "1422_324242_S.jpg",
+            },{
+                subid:5,
+                code: 25,
+                name: "name15",
+                img: "1472_2484952_S.jpg",
+            },{
+                subid:6,
+                code: 26,
+                name: "name16",
+                img: "58294_8161183_S.jpg",
+            }
+        ]
+    },
+];
